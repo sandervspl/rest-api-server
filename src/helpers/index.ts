8248d495d9e2c83010bb0ef5f2506f44f9c10457
Sander Vispoel <sandervispoel@hotmail.com>
2019-01-19 18:29:10 +0100

    Fix build

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,3 @@
 export { generateRandomString } from './generateRandomString';
-export { routesTable } from './generateRoutesTable';
 export { jwtVerify } from './jwtVerify';
 export { UUID } from './UUID';
