0ef154daf3c2a5165baf76943533bf91e7a7d96f
Sander Vispoel <sandervispoel@hotmail.com>
2019-01-19 19:35:25 +0100

    Remove unused files

--- src/helpers/generateRandomString.ts
+++
@@ -1,8 +0,0 @@
-export const generateRandomString = (length: number) => {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  return Array.from({ length }).reduce(
-    text => (text += possible.charAt(Math.floor(Math.random() * possible.length))),
-    ''
-  );
-};
--- src/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export { generateRandomString } from './generateRandomString';
-export { jwtVerify } from './jwtVerify';
-export { UUID } from './UUID';
